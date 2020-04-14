894cd87d913d7d8d0770a6143dadd2abfe6bb82e
Kevin Boston <kbos22@gmail.com>
2020-04-14 20:21:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -123,19 +123,23 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
-  // implement me!
-  /**
-   * This function should move DODGER to the left
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+  window.requestAnimationFrame(function() {
+    const left = positionToInteger(DODGER.style.left);
+
+    if (left > 0) {
+      DODGER.style.left = `${left - 4}px`;
+    }
+  })
 }
 
 function moveDodgerRight() {
-  // implement me!
-  /**
-   * This function should move DODGER to the right
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+ window.requestAnimationFrame(function(){
+   const right = positionToInteger(DODGER.style.right);
+   
+   if (right > 0) {
+     DODGER.style.right = `${right-4}px`;
+   }
+ })
 }
 
 /**
